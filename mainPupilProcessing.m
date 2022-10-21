6b37ee1555447fc656ae300b44cccea296a3e399
ngf7 <58956003+ngf7@users.noreply.github.com>
2022-10-21 10:11:30 -0400

    user input required for laser on/off frame

--- mainPupilProcessing.m
+++ mainPupilProcessing.m
@@ -156,10 +156,33 @@ for block =blocks
       center_column = [center_column center(1,1)];
     end
     
-    % manipulations on the raw trace of current block
+    % manipulations on the raw trace of current block - chopping to laser
     raw_radii(raw_radii<10)=0;
     first_index = find(raw_radii,1,'first'); %2p acquisition onset
     last_index = find(raw_radii,1,'last'); %2p offset
+
+    figure(1);clf
+    imshow(read(obj,first_index))
+    title(strcat('Frame #',num2str(first_index)))
+    correct_start=input('Does this look like the correct start frame? 1/0');
+
+    if correct_start==0
+        start=input('What is the correct galvo start frame?');
+        first_index = start;
+    end
+
+
+    figure(1);clf
+    imshow(read(obj,last_index))
+    title(strcat('Frame #',num2str(first_index)))
+    correct_end=input('Does this look like the correct end frame? 1/0');
+
+    if correct_end==0
+        last=input('What is the correct galvo end frame?');
+        last_index = last;
+    end
+
+
     the_radii_cut = raw_radii(first_index:last_index);
     center_row_cut = center_row(first_index:last_index);
     center_column_cut = center_column(first_index:last_index);
